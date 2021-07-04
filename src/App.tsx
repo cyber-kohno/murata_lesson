bfc361e1802dfa881ba8863ae41927610aca30b9
unknown <kohno@cyberbeing.co.jp>
2021-07-04 11:36:33 +0900

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+import Sample1 from 'app/sample/sample1/Sample1';
+import { VFC } from 'react';
+
+const App: VFC = () => {
+  return (
+    <Sample1 />
+  )
+}
+
+export default App;
---
+++ src/app/sample/sample1/Sample1.tsx
@@ -0,0 +1,10 @@
+import { VFC } from "react";
+
+const Sample1: VFC = () => {
+
+    return (
+        <p>Hello react!</p>
+    );
+}
+
+export default Sample1;
